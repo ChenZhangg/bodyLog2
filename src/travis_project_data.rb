366038c52dedc4c4bbd92aa7b90972578c8d272e
zc <340355960@qq.com>
2018-03-25 16:31:29 +0800

    9:18

--- src/travis_project_data.rb
+++ src/travis_project_data.rb
@@ -158,7 +158,7 @@ def insertData(hash)
   end
 end
 
-CLIENT = Mysql2::Client.new(:host => 'localhost', :username => 'root',:password=>'root',:encoding => 'utf8mb4')
+CLIENT = Mysql2::Client.new(:host => 'localhost', :username => 'root',:password=>'root',:encoding => 'utf8mb4',:reconnect => true)
 CLIENT.query('ALTER DATABASE zc CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;')
 CLIENT.query('USE zc')
 CLIENT.query('ALTER Table zc.travis0 CONVERT TO CHARACTER SET utf8;')
