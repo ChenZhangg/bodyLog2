688cbd7060542a519b49a6f4e04b8e43e54c5b21
zc <340355960@qq.com>
2018-04-09 20:18:03 +0800

    19:54

--- src/parse_json.rb
+++ src/parse_json.rb
@@ -14,50 +14,54 @@ class JavaRepoBuildDatum < ActiveRecord::Base
 end
 
 def parse_job_json_file(job_file_path)
-  puts job_file_path
-  j = JSON.parse IO.read(job_file_path)
-  hash = Hash.new
-  hash[:repo_name] = j['repository']['slug']
-  hash[:job_id] = j['id']
-  hash[:job_allow_failure] = j['allow_failure']
-  hash[:job_number] = j['number']
-  hash[:job_state] = j['state']
-  hash[:job_started_at] = j['started_at']
-  hash[:job_finished_at] = j['finished_at']
-  hash[:job_queue] = j['queue']
-  hash[:job_stage] = j['stage']
-  hash[:job_created_at] = j['created_at']
-  hash[:job_updated_at] = j['updated_at']
+  begin
+    j = JSON.parse IO.read(job_file_path)
+    hash = Hash.new
+    hash[:repo_name] = j['repository']['slug']
+    hash[:job_id] = j['id']
+    hash[:job_allow_failure] = j['allow_failure']
+    hash[:job_number] = j['number']
+    hash[:job_state] = j['state']
+    hash[:job_started_at] = j['started_at']
+    hash[:job_finished_at] = j['finished_at']
+    hash[:job_queue] = j['queue']
+    hash[:job_stage] = j['stage']
+    hash[:job_created_at] = j['created_at']
+    hash[:job_updated_at] = j['updated_at']
 
-  hash[:commit_id] = j['commit'] ? j['commit']['id'] : nil
-  hash[:commit_sha] = j['commit'] ? j['commit']['sha'] : nil
-  hash[:commit_ref] = j['commit'] ? j['commit']['ref'] : nil
-  hash[:commit_message] = j['commit'] ? j['commit']['message'] : nil
-  hash[:commit_compare_url] = j['commit'] ? j['commit']['compare_url'] : nil
-  hash[:commit_committed_at] = j['commit'] ? j['commit']['committed_at'] : nil
+    hash[:commit_id] = j['commit'] ? j['commit']['id'] : nil
+    hash[:commit_sha] = j['commit'] ? j['commit']['sha'] : nil
+    hash[:commit_ref] = j['commit'] ? j['commit']['ref'] : nil
+    hash[:commit_message] = j['commit'] ? j['commit']['message'] : nil
+    hash[:commit_compare_url] = j['commit'] ? j['commit']['compare_url'] : nil
+    hash[:commit_committed_at] = j['commit'] ? j['commit']['committed_at'] : nil
 
-  build_file_path = job_file_path.sub(/(?<=\/)job(?=@)/, 'build').sub(/(?<=\d)@\d+(?=\.json)/, '')
-  puts build_file_path
-  j = JSON.parse IO.read(build_file_path)
+    build_file_path = job_file_path.sub(/(?<=\/)job(?=@)/, 'build').sub(/(?<=\d)@\d+(?=\.json)/, '')
+    j = JSON.parse IO.read(build_file_path)
 
-  hash[:build_id] = j['id']
-  hash[:build_number] = j['number']
-  hash[:build_state] = j['state']
-  hash[:build_duration] = j['duration']
-  hash[:build_event_type] = j['event_type']
-  hash[:pull_request_title] = j['pull_request_title']
-  hash[:pull_request_number] = j['pull_request_number']
-  hash[:build_started_at] = j['started_at']
-  hash[:build_finished_at] = j['finished_at']
-  hash[:build_branch] = j['branch'] ? j['branch']['name'] : nil
-  hash[:build_tag] = j['tag'] ? j['tag']['name'] : nil
+    hash[:build_id] = j['id']
+    hash[:build_number] = j['number']
+    hash[:build_state] = j['state']
+    hash[:build_duration] = j['duration']
+    hash[:build_event_type] = j['event_type']
+    hash[:pull_request_title] = j['pull_request_title']
+    hash[:pull_request_number] = j['pull_request_number']
+    hash[:build_started_at] = j['started_at']
+    hash[:build_finished_at] = j['finished_at']
+    hash[:build_branch] = j['branch'] ? j['branch']['name'] : nil
+    hash[:build_tag] = j['tag'] ? j['tag']['name'] : nil
 
-  hash[:build_stages] = j['stages']
-  hash[:created_by_id] = j['created_by'] ? j['created_by']['id'] : nil
-  hash[:created_by_login] = j['created_by'] ? j['created_by']['login'] : nil
-  hash[:build_updated_at] = j['updated_at']
-  @queue.enq hash
-  #puts JSON.pretty_generate(j)
+    hash[:build_stages] = j['stages']
+    hash[:created_by_id] = j['created_by'] ? j['created_by']['id'] : nil
+    hash[:created_by_login] = j['created_by'] ? j['created_by']['login'] : nil
+    hash[:build_updated_at] = j['updated_at']
+    @queue.enq hash
+    #puts JSON.pretty_generate(j)
+  rescue
+    puts  $!
+    puts job_file_path
+    raise $!
+  end
 end
 
 def scan_json_files(json_files_path)
@@ -72,7 +76,7 @@ def scan_json_files(json_files_path)
       threads<<thr
       loop do
         count = Thread.list.count{|thread| thread.alive? }
-        break if count <= 100
+        break if count <= 50
       end
       threads.delete_if{ |thread| !thread.alive? }
     end
