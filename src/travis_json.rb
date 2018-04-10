b82d719cdf00baa0b3ca1f9c401acee8a4b9686d
zc <340355960@qq.com>
2018-04-10 14:59:19 +0800

    9:10

--- src/travis_json.rb
+++ src/travis_json.rb
@@ -86,7 +86,7 @@ def get_builds_list(repo_id, offset, parent_dir)
     end
   end
   p "#{parent_dir}  over"
-  Thread.list.each { |thread| thread.join if thread.alive? && thread != Thread.current}
+  #Thread.list.each { |thread| thread.join if thread.alive? && thread != Thread.current}
 end
 
 def get_repo_id(repo_name, parent_dir)
@@ -103,7 +103,7 @@ def get_repo_id(repo_name, parent_dir)
     return
   end
   id = j['id']
-  get_builds_list(id, 300, parent_dir)
+  get_builds_list(id, 450, parent_dir)
   #puts JSON.pretty_generate(j)
 end
 
