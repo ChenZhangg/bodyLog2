92f763e2a3406ce7a5c08282d019b16da616abf4
zc <340355960@qq.com>
2018-05-04 12:52:44 +0800

    12:52

--- src/travis_log.rb
+++ src/travis_log.rb
@@ -54,7 +54,7 @@ end
 def scan_json_files(json_files_path)
 
   flag = true
-  flag_repo = 'OpenNTF@SocialSDK'
+  flag_repo = 'apache@parquet-mr'
   Dir.entries(json_files_path).select{ |p| p =~ /.+@.+/ }.sort_by!{ |e| File.mtime(File.join(json_files_path, e)) }.each do |repo_name|
     puts "Scan project #{repo_name}"
     flag = false if repo_name.include? flag_repo
