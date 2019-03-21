0a70425f9c597b1933a9f8322a0d2541b8c96f09
compilererrorinci <340355960@qq.com>
2019-03-21 20:36:31 +0800

    parse build

--- src/parse_only_build_json.rb
+++ src/parse_only_build_json.rb
@@ -17,7 +17,7 @@ class Build < ActiveRecord::Base
   )
 end
 
-class TravisJavaRepository < ActiveRecord::Base
+class Repository < ActiveRecord::Base
   establish_connection(
       adapter:  "mysql",
       host:     "10.131.252.160",
@@ -28,36 +28,48 @@ class TravisJavaRepository < ActiveRecord::Base
       collation: "utf8mb4_bin"
   )
 end
-
-def rescue_build(repo_name, build_number, hash)
+#rsync -av fdse@10.141.221.86:/home/fdse/user/zc/bodyLog2/  /home/fdse/user/zc/bodyLog2 
+def rescue_build(repo_slug, build_number, hash)
   count = 0
   begin
-    trepo = Travis::Repository.find(repo_name)
+    trepo = Travis::Repository.find(repo_slug)
     tbuild = trepo.build(build_number)
-    hash[:build_state] = tbuild.state
     hash[:build_id] = tbuild.id
-    hash[:build_duration] = tbuild.duration
-    hash[:build_event_type] = tbuild.push? ? "push" : "pull_request"
+
+    hash[:state] = tbuild.state
+    hash[:duration] = tbuild.duration
+    hash[:event_type] = tbuild.push? ? "push" : nil
+
     hash[:pull_request_title] = tbuild.pull_request_title
     hash[:pull_request_number] = tbuild.pull_request_number
-    hash[:build_started_at] = tbuild.started_at ? DateTime.parse(tbuild.started_at.to_s).new_offset(0) : nil
-    hash[:build_finished_at] = tbuild.finished_at ? DateTime.parse(tbuild.finished_at.to_s).new_offset(0) : nil
-    hash[:build_branch] = tbuild.branch_info
-    hash[:build_tag] = nil#j['tag'] ? j['tag']['name'] : nil
+    hash[:started_at] = tbuild.started_at ? DateTime.parse(tbuild.started_at.to_s).new_offset(0) : nil
+    hash[:finished_at] = tbuild.finished_at ? DateTime.parse(tbuild.finished_at.to_s).new_offset(0) : nil
 
-    hash[:build_stages] =  nil#j['stages']
-    hash[:created_by_id] = nil#j['created_by'] ? j['created_by']['id'] : nil
-    hash[:created_by_login] = nil#j['created_by'] ? j['created_by']['login'] : nil
-    hash[:build_updated_at] = nil#j['updated_at']
+    hash[:repo_id] = trepo.id
+    hash[:repo_name] = trepo.name
+    hash[:repo_slug] = trepo.slug
+
+    hash[:branch_name] = tbuild.branch_info
+    hash[:tag] = nil#j['tag'] ? j['tag']['name'] : nil
+
 
     commit = tbuild.commit
     hash[:commit_id] =  tbuild.commit_id
     hash[:commit_sha] =  commit.sha
-    hash[:commit_ref] = commit.branch
+    hash[:commit_ref] = 'refs/heads/' + commit.branch
     hash[:commit_message] =  commit.message
     hash[:commit_compare_url] =  commit.compare_url
     hash[:commit_committed_at] = commit.committed_at ? DateTime.parse(commit.committed_at.to_s).new_offset(0) : nil
+
+    hash[:jobs] = tbuild.jobs
     hash[:jobs_number] = tbuild.jobs.length
+
+    hash[:build_stages] =  nil#j['stages']
+    hash[:user_id] = nil#j['created_by'] ? j['created_by']['id'] : nil
+    hash[:user_login] = nil#j['created_by'] ? j['created_by']['login'] : nil
+    hash[:pdated_at] = nil#j['updated_at']
+
+    hash[:parse_error_flag] = true
   rescue
     count += 1
     sleep 5
@@ -65,33 +77,36 @@ def rescue_build(repo_name, build_number, hash)
   end
 end
 
-def parse_build_json_file(build_file_path, repo_id)
+def parse_build_json_file(build_file_path, repository_id)
   hash = Hash.new
-  hash[:travis_java_repository_id] = repo_id
+  hash[:repository_id] = repository_id
   match = /json_files\/(.+)\/build@(.+)\.json/.match build_file_path
   return unless match
   repo_name, build_number = match[1].sub(/@/, '/'), match[2].to_i
-  hash[:repo_name] = repo_name
-  hash[:build_number] = build_number
-  hash[:travis_java_repository_id] =repo_id
+  hash[:repo_slug] = repo_name
+  hash[:number] = build_number
+  hash[:repository_id] = repository_id
   begin
     j = JSON.parse IO.read(build_file_path)
-    hash[:build_state] = j['state']
     hash[:build_id] = j['id']
-    hash[:build_duration] = j['duration']
-    hash[:build_event_type] = j['event_type']
-    hash[:build_previous_state] = j['previous_state']
+    hash[:repository_id] = repository_id
+    hash[:number] = j['number'].to_i
+    hash[:state] = j['state']
+    hash[:duration] = j['duration']
+    hash[:event_type] = j['event_type']
+    hash[:previous_state] = j['previous_state']
     hash[:pull_request_title] = j['pull_request_title']
-    hash[:pull_request_number] = j['pull_request_number']
-    hash[:build_started_at] = j['started_at'] ? DateTime.parse(j['started_at']).new_offset(0) : nil
+    hash[:pull_request_number] = j['pull_request_number'].to_i
+    hash[:started_at] = j['started_at'] ? DateTime.parse(j['started_at']).new_offset(0) : nil
     hash[:build_finished_at] = j['finished_at'] ? DateTime.parse(j['finished_at']).new_offset(0) : nil
-    hash[:build_branch] = j['branch'] ? j['branch']['name'] : nil
-    hash[:build_tag] = j['tag']
 
-    hash[:build_stages] = j['stages']
-    hash[:created_by_id] = j['created_by'] ? j['created_by']['id'] : nil
-    hash[:created_by_login] = j['created_by'] ? j['created_by']['login'] : nil
-    hash[:build_updated_at] = j['updated_at'] ? DateTime.parse(j['updated_at']).new_offset(0) : nil
+    hash[:repo_id] = j['repository'] ? j['repository']['id'] : nil
+    hash[:repo_name] = j['repository'] ? j['repository']['name'] : nil
+    hash[:repo_slug] = j['repository'] ? j['repository']['slug'] : nil
+
+    hash[:build_branch] = j['branch'] ? j['branch']['name'] : nil
+
+    hash[:tag] = j['tag']
 
     hash[:commit_id] = j['commit'] ? j['commit']['id'] : nil
     hash[:commit_sha] = j['commit'] ? j['commit']['sha'] : nil
@@ -99,19 +114,29 @@ def parse_build_json_file(build_file_path, repo_id)
     hash[:commit_message] = j['commit'] ? j['commit']['message'] : nil
     hash[:commit_compare_url] = j['commit'] ? j['commit']['compare_url'] : nil
     hash[:commit_committed_at] = (j['commit'] && j['commit']['committed_at']) ? DateTime.parse(j['commit']['committed_at']).new_offset(0) : nil
+
+    hash[:jobs] = j['jobs']
     hash[:jobs_number] = j['jobs'].length
+
+    hash[:stages] = j['stages']
+
+    hash[:user_id] = j['created_by'] ? j['created_by']['id'] : nil
+    hash[:user_login] = j['created_by'] ? j['created_by']['login'] : nil
+
+    hash[:build_updated_at] = j['updated_at'] ? DateTime.parse(j['updated_at']).new_offset(0) : nil
+    hash[:parse_error_flag] = false
   rescue
     puts  $!
     puts $@
     puts build_file_path
-    rescue_build(repo_name, build_number, hash)
+    rescue_build(repo_slug, build_number, hash)
   end
   @result_queue.enq hash
 end
 
 def thread_init
   consumer = Thread.new do
-    id = 2858077
+    id = 0
     loop do
       bulk = []
       hash = nil
@@ -120,9 +145,9 @@ def thread_init
         break if hash == :END_OF_WORK
         id += 1
         hash[:id] = id
-        bulk << JavaRepoBuildDatum.new(hash)
+        bulk << Build.new(hash)
       end
-      JavaRepoBuildDatum.import bulk
+      Build.import bulk
       break if hash == :END_OF_WORK
     end
   end
@@ -133,7 +158,7 @@ def thread_init
       loop do
         h = @input_queue.deq
         break if h == :END_OF_WORK
-        parse_build_json_file h[:build_file_path], h[:repo_id]
+        parse_build_json_file h[:build_file_path], h[:repository_id]
       end
     end
     threads << thread
@@ -144,17 +169,16 @@ end
 def scan_json_files(json_files_path)
   consumer, threads = thread_init
 
-  TravisJavaRepository.where("id >= ? AND builds >= ? AND stars>= ?", 1729498, 50, 25).find_each do |repo|
+  Repository.where("id >= ? AND builds >= ? AND stars>= ?", 0, 50, 25).find_each do |repo|
     puts "Scan repo: #{repo.repo_name}"
     repo_path = File.join(json_files_path, repo.repo_name.sub(/\//, '@'))
-    repo_id = repo.id
     Dir.foreach(repo_path) do |build_file_name|
       next if build_file_name !~ /build@.+/
       build_file_path = File.join(repo_path, build_file_name)
       p build_file_path
       hash = Hash.new
       hash[:build_file_path] = build_file_path
-      hash[:repo_id] = repo_id
+      hash[:repository_id] = repo.id
       @input_queue.enq hash
     end
   end
--- src/test.rb
+++ src/test.rb
@@ -1,8 +1,17 @@
 require 'open-uri'
 require 'json'
+require 'travis'
+=begin
 url = "https://api.travis-ci.org/user/724655"
 f = open(url,'Travis-API-Version'=>'3','Authorization'=>'token C-cYiDyx1DUXq3rjwWXmoQ')
 j= JSON.parse f.read
 #p j
 puts JSON.pretty_generate(j)
 #https://api.github.com/users/lpereir4ruby
+=end
+trepo = Travis::Repository.find('rails/rails')
+p trepo.id
+p trepo.name
+p trepo.slug
+tbuild = trepo.build('1000')
+p tbuild.jobs
